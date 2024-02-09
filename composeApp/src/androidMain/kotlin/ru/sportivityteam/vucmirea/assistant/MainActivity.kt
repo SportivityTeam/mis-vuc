89e5a637702b8ce7170a82c86a3e13e031e2fdc3
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-09 17:10:57 +0300

    set root screen to splash screen

--- composeApp/src/androidMain/kotlin/ru/sportivityteam/vucmirea/assistant/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/ru/sportivityteam/vucmirea/assistant/MainActivity.kt
@@ -3,19 +3,15 @@ package ru.sportivityteam.vucmirea.assistant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.alexgladkov.odyssey.compose.setup.OdysseyConfiguration
-import ru.alexgladkov.odyssey.compose.setup.setNavigationContent
-import ru.sportivityteam.vucmirea.assistant.presentation.navigation.navigationGraph
+import cafe.adriel.voyager.navigator.Navigator
+import ru.sportivityteam.vucmirea.assistant.presentation.screens.splash.SplashScreen
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val configuration = OdysseyConfiguration(canvas = this)
-            setNavigationContent(configuration = configuration) {
-                navigationGraph()
-            }
+            App()
         }
     }
 }
