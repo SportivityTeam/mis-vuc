e2efdabf6e86335fec82c8851a5f332060c38029
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-10 12:46:32 +0300

    added base screen class

---
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/ui/component/BaseScreen.kt
@@ -0,0 +1,35 @@
+package ru.sportivityteam.vucmirea.assistant.presentation.ui.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.screen.Screen
+import ru.sportivityteam.vucmirea.assistant.theme.AppTheme
+
+abstract class BaseScreen : Screen {
+    @Composable
+    override fun Content() {
+        AppTheme {
+            CompositionLocalProvider {
+                Box(
+                    contentAlignment = Alignment.TopCenter,
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .width(600.dp)
+                    ) {
+                        ScreenContent()
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    abstract fun ScreenContent()
+}
