1cbaf58ca0cb8907ac49f861afafc4b6e2d86fa5
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-14 14:01:43 +0300

    feature: added material dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,7 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.material3)
+            implementation(compose.material)
             implementation(compose.materialIconsExtended)
             @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
             implementation(compose.components.resources)
