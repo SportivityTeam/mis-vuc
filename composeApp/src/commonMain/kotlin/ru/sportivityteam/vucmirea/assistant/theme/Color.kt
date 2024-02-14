80ad36a9d074fe19222202836c89f470cacc6d27
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-14 14:02:50 +0300

    style: changed colors

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/theme/Color.kt
@@ -1,15 +1,14 @@
 package ru.sportivityteam.vucmirea.assistant.theme
 
 import androidx.compose.ui.graphics.Color
-import ru.sportivityteam.vucmirea.assistant.MR
 
 internal val basePalette = AssistantColors(
-    primary = Color(MR.colors.primary.resourceId),
+    primary = Color(0xFF000353),
     secondary = Color(0xFF131315),
     tertiary = Color(0xFF1B1B1B),
-    accent = Color(0xFFEAFAB1),
+    accent = Color(0xFFE77D00),
     white = Color(0xFFFFFFFF),
     disable = Color(0xFF525252),
     secondaryText = Color(0xFF8C8C8C),
-    accentDisabled = Color(0x99EAFAB1)
+    accentDisabled = Color(0x99E77D00)
 )
