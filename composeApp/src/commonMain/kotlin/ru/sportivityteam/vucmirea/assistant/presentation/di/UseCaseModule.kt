1a3400d629fcda81fe948126c6e075b284869c48
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-14 14:03:33 +0300

    feat: added strings field to base use case class

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/di/UseCaseModule.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/di/UseCaseModule.kt
@@ -1,9 +1,12 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.di
 
 import org.koin.dsl.module
+import ru.sportivityteam.vucmirea.assistant.domain.usecase.auth.AuthUserUseCase
+import ru.sportivityteam.vucmirea.assistant.domain.usecase.auth.AuthUserUseCaseImpl
 import ru.sportivityteam.vucmirea.assistant.domain.usecase.auth.GetUserLoginUseCase
 import ru.sportivityteam.vucmirea.assistant.domain.usecase.auth.GetUserLoginUseCaseImpl
 
 internal fun useCaseModule() = module {
     factory<GetUserLoginUseCase> { GetUserLoginUseCaseImpl(get()) }
+    factory<AuthUserUseCase> { AuthUserUseCaseImpl(get()) }
 }
