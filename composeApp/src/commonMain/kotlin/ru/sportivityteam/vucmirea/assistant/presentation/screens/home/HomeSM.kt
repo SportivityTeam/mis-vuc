54a432f61aa3f53d4ac3fc2b109054280ce62fce
SogYa <Ghost.Spartan36@gmail.com>
2024-02-15 12:15:30 +0300

    feat: added base realization and view states

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeSM.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeSM.kt
@@ -1,4 +1,11 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.screens.home
 
-class HomeSM {
+import ru.sportivityteam.vucmirea.assistant.presentation.ui.mvi.BaseStateScreenModel
+
+class HomeSM: BaseStateScreenModel<HomeViewState, HomeViewAction, HomeViewEvent>(initialState = HomeViewState()) {
+    override fun obtainEvent(viewEvent: HomeViewEvent) {
+        when(viewEvent){
+            is HomeViewEvent.OpenLesson -> viewEvent.lessonId
+        }
+    }
 }
--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.screens.home
 
+import androidx.compose.runtime.Composable
 import ru.sportivityteam.vucmirea.assistant.presentation.ui.component.BaseScreen
 
 class HomeScreen : BaseScreen(){
+    @Composable
     override fun ScreenContent() {
         TODO("Not yet implemented")
     }
