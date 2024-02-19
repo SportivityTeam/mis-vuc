bb1207360089ff1754b5c47b08401fa8f969e1fd
SogYa <Ghost.Spartan36@gmail.com>
2024-02-19 19:48:05 +0300

    style: changed scaffold from material to material3

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
@@ -2,7 +2,7 @@ package ru.sportivityteam.vucmirea.assistant.presentation.screens.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.tab.CurrentTab
