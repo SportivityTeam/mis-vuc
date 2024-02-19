3ba4f45ad7a217c8de5a409a0f3e72d33695422c
SogYa <Ghost.Spartan36@gmail.com>
2024-02-19 19:46:23 +0300

    feat: added bottom navigation to home screen

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/HomeScreen.kt
@@ -1,11 +1,30 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.screens.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import ru.sportivityteam.vucmirea.assistant.presentation.navigation.BottomBar
+import ru.sportivityteam.vucmirea.assistant.presentation.navigation.TimetableTab
 import ru.sportivityteam.vucmirea.assistant.presentation.ui.component.BaseScreen
 
-class HomeScreen : BaseScreen(){
+class HomeScreen : BaseScreen() {
     @Composable
     override fun ScreenContent() {
-        TODO("Not yet implemented")
+        TabNavigator(TimetableTab) {
+            Scaffold(
+                content = {
+                    Box(modifier = Modifier.padding(it)) {
+                        CurrentTab()
+                    }
+                },
+                bottomBar = {
+                    BottomBar()
+                }
+            )
+        }
     }
 }
