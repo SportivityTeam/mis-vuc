e057a3176384acdab4361957d765a6e2e539f366
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-20 10:58:57 +0300

    feat: added test screen realization

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/timetable/TimetableScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/timetable/TimetableScreen.kt
@@ -1,11 +1,17 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.screens.home.timetable
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ru.sportivityteam.vucmirea.assistant.presentation.ui.component.BaseScreen
 
 class TimetableScreen : BaseScreen() {
     @Composable
     override fun ScreenContent() {
-        TODO("Not yet implemented")
+        Box(modifier = Modifier.fillMaxSize()){
+            Text(text = "Распсиание")
+        }
     }
 }
