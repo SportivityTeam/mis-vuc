0f3323ee5093e7936b86e3d20be94d9c453d1c46
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-20 10:57:07 +0300

    feat: added settings screen

---
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/settings/SettingsScreen.kt
@@ -0,0 +1,11 @@
+package ru.sportivityteam.vucmirea.assistant.presentation.screens.settings
+
+import androidx.compose.runtime.Composable
+import ru.sportivityteam.vucmirea.assistant.presentation.ui.component.BaseScreen
+
+class SettingsScreen : BaseScreen() {
+    @Composable
+    override fun ScreenContent() {
+
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/settings/SettingsStates.kt
@@ -0,0 +1,17 @@
+package ru.sportivityteam.vucmirea.assistant.presentation.screens.settings
+
+data class HomeViewState(
+    val isLoading: Boolean = false,
+    val groupNumber: String = "",
+    val date: String = "",
+    val lessons: List<Unit> = listOf()
+)
+
+sealed class HomeViewAction {
+    data object NavigateToSettings : HomeViewAction()
+    data class NavigateToLesson(val lessonId: String) : HomeViewAction()
+}
+
+sealed class HomeViewEvent {
+    data class OpenLesson(val lessonId: String) : HomeViewEvent()
+}
