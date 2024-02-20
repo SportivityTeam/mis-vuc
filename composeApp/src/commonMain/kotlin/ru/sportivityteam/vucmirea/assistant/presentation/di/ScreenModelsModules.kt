38b69faf9ea1f5e90e5bedc6dbb0122dcd8a38c7
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-20 11:56:29 +0300

    feat: added new screenModels to sm module

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/di/ScreenModelsModules.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/di/ScreenModelsModules.kt
@@ -2,9 +2,17 @@ package ru.sportivityteam.vucmirea.assistant.presentation.di
 
 import org.koin.dsl.module
 import ru.sportivityteam.vucmirea.assistant.presentation.screens.auth.AuthSM
+import ru.sportivityteam.vucmirea.assistant.presentation.screens.home.employee.EmployeeSM
+import ru.sportivityteam.vucmirea.assistant.presentation.screens.home.info.InfoSM
+import ru.sportivityteam.vucmirea.assistant.presentation.screens.home.timetable.TimetableSM
+import ru.sportivityteam.vucmirea.assistant.presentation.screens.settings.SettingsSM
 import ru.sportivityteam.vucmirea.assistant.presentation.screens.splash.SplashSM
 
 internal fun screenModelsModule() = module {
     factory { SplashSM(get()) }
     factory { AuthSM(get()) }
+    factory { TimetableSM() }
+    factory { EmployeeSM() }
+    factory { InfoSM() }
+    factory { SettingsSM() }
 }
