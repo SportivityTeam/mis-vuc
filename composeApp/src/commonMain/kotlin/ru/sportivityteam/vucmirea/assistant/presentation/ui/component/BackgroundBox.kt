91c30cb06b5637e3f10107cdd8f5476e193e72b5
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-02-20 11:56:52 +0300

    feat: added custom fields

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/ui/component/BackgroundBox.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/ui/component/BackgroundBox.kt
@@ -10,17 +10,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ru.sportivityteam.vucmirea.assistant.MR
+import ru.sportivityteam.vucmirea.assistant.theme.AssistantTheme
 
 @Composable
-fun BackgroundBox(content: @Composable (ColumnScope.() -> Unit)) = Box(
+fun BackgroundBox(
+    paddingTop: Dp = 25.dp,
+    horizontalAlignment: Alignment.Horizontal = Alignment.CenterHorizontally,
+    content: @Composable (ColumnScope.() -> Unit)
+) = Box(
     Modifier
         .fillMaxSize()
         .padding(horizontal = 15.dp)
-        .padding(top = 25.dp, bottom = 50.dp)
+        .padding(top = paddingTop, bottom = 50.dp)
+        .clip(AssistantTheme.shape.cornersStyle)
 ) {
     Image(
         modifier = Modifier.fillMaxSize(),
@@ -29,7 +37,7 @@ fun BackgroundBox(content: @Composable (ColumnScope.() -> Unit)) = Box(
         contentDescription = null,
     )
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = horizontalAlignment,
         verticalArrangement = Arrangement.Top
     ) {
         content()
