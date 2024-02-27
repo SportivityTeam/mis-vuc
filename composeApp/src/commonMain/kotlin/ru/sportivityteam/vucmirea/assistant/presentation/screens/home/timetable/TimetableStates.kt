90f87f1e75502346c260eda29600f11c7a0e9cd6
SogYa <Ghost.Spartan36@gmail.com>
2024-02-27 22:08:50 +0300

    feat: added test data

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/timetable/TimetableStates.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/timetable/TimetableStates.kt
@@ -1,17 +1,45 @@
 package ru.sportivityteam.vucmirea.assistant.presentation.screens.home.timetable
 
-data class HomeViewState(
+import ru.sportivityteam.vucmirea.assistant.presentation.entity.LessonPresentation
+
+data class TimetableViewState(
     val isLoading: Boolean = false,
-    val groupNumber: String = "",
-    val date: String = "",
-    val lessons: List<Unit> = listOf()
+    val day: String = "Пятница",
+    val date: String = "14 октября",
+    val lessons: List<LessonPresentation> = listOf(
+        LessonPresentation(
+            "2323",
+            audience = "Ауд. 101",
+            lesson = "ВСП",
+            employee = "Ренкавик В.А.",
+            lessonTime = "09:00-10:30"
+        ),LessonPresentation(
+            "2323",
+            audience = "Ауд. 101",
+            lesson = "ВСП",
+            employee = "Ренкавик В.А.",
+            lessonTime = "10:40-12:10"
+        ),LessonPresentation(
+            "2323",
+            audience = "Ауд. 101",
+            lesson = "ВСП",
+            employee = "Ренкавик В.А.",
+            lessonTime = "13:00-14:30"
+        ),
+        LessonPresentation(
+            "2323",
+            audience = "Ауд. 101",
+            lesson = "ВСП",
+            employee = "Ренкавик В.А.",
+            lessonTime = "14:40-16:10"
+        ),
+    )
 )
 
-sealed class HomeViewAction {
-    data object NavigateToSettings : HomeViewAction()
-    data class NavigateToLesson(val lessonId: String) : HomeViewAction()
+sealed class TimetableViewAction {
+    data class NavigateToLesson(val lessonId: String) : TimetableViewAction()
 }
 
-sealed class HomeViewEvent {
-    data class OpenLesson(val lessonId: String) : HomeViewEvent()
+sealed class TimetableViewEvent {
+    data class OpenLesson(val lessonId: String) : TimetableViewEvent()
 }
