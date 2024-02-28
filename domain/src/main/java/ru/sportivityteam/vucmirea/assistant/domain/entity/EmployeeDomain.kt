94a5935d37d3c1960dbda28b678fccd508e33ce9
SogYa <Ghost.Spartan36@gmail.com>
2024-02-28 11:43:38 +0300

    feature: added employee entities

---
+++ domain/src/main/java/ru/sportivityteam/vucmirea/assistant/domain/entity/EmployeeDomain.kt
@@ -0,0 +1,11 @@
+package ru.sportivityteam.vucmirea.assistant.domain.entity
+
+interface EmployeeDomain {
+    val imageUri: String
+    val fullName: String
+    val jobTitle: String
+    val email: String
+    val phone: String
+    val phoneAdd: String
+    val audience: String
+}
