c69a75eea71c145fc5938339a71314cccff63157
SogYa <Ghost.Spartan36@gmail.com>
2024-02-28 13:21:21 +0300

    feature: added base screen realization

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/employee/EmployeeSM.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/presentation/screens/home/employee/EmployeeSM.kt
@@ -2,10 +2,8 @@ package ru.sportivityteam.vucmirea.assistant.presentation.screens.home.employee
 
 import ru.sportivityteam.vucmirea.assistant.presentation.ui.mvi.BaseStateScreenModel
 
-class EmployeeSM: BaseStateScreenModel<HomeViewState, HomeViewAction, HomeViewEvent>(initialState = HomeViewState()) {
-    override fun obtainEvent(viewEvent: HomeViewEvent) {
-        when(viewEvent){
-            is HomeViewEvent.OpenLesson -> viewEvent.lessonId
-        }
+class EmployeeSM: BaseStateScreenModel<EmployeeViewState, EmployeeViewAction, EmployeeViewEvent>(initialState = EmployeeViewState()) {
+    override fun obtainEvent(viewEvent: EmployeeViewEvent) {
+
     }
 }
