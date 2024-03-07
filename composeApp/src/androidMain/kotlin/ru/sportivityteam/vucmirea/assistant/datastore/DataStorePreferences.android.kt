6f1735707f158a1bc345967a92eabffbc6a31271
Timofey Sorokin <Ghost.Spartan36@gmail.com>
2024-03-07 13:12:39 +0300

    feat: added data store functions to all platforms

---
+++ composeApp/src/androidMain/kotlin/ru/sportivityteam/vucmirea/assistant/datastore/DataStorePreferences.android.kt
@@ -0,0 +1,24 @@
+package ru.sportivityteam.vucmirea.assistant.datastore
+
+import androidx.datastore.core.DataMigration
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineScope
+import ru.sportivityteam.vucmirea.assistant.data.datastore.SETTINGS_PREFERENCES
+import ru.sportivityteam.vucmirea.assistant.data.datastore.createDataStoreWithDefaults
+import ru.sportivityteam.vucmirea.assistant.util.applicationContext
+import java.io.File
+
+actual fun dataStorePreferences(
+    corruptionHandler: ReplaceFileCorruptionHandler<Preferences>?,
+    coroutineScope: CoroutineScope,
+    migrations: List<DataMigration<Preferences>>,
+): DataStore<Preferences> = createDataStoreWithDefaults(
+    corruptionHandler = corruptionHandler,
+    migrations = migrations,
+    coroutineScope = coroutineScope,
+    path = {
+        File(applicationContext.filesDir, "datastore/$SETTINGS_PREFERENCES").path
+    }
+)
---
+++ composeApp/src/iosMain/kotlin/datastore/DataStorePreferences.ios.kt
@@ -0,0 +1,30 @@
+import androidx.datastore.core.DataMigration
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import okio.Path.Companion.toPath
+
+@OptIn(ExperimentalForeignApi::class)
+actual fun dataStorePreferences(
+    corruptionHandler: ReplaceFileCorruptionHandler<Preferences>?,
+    coroutineScope: CoroutineScope,
+    migrations: List<DataMigration<Preferences>>,
+): DataStore<Preferences> = createDataStoreWithDefaults(
+    corruptionHandler = corruptionHandler,
+    migrations = migrations,
+    coroutineScope = coroutineScope,
+    path = {
+        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null,
+        )
+        (requireNotNull(documentDirectory).path + "/$SETTINGS_PREFERENCES")
+    }
+)
