c2b6d3b2f0374cdfe632b9e6f3e2cbed99ad0bec
SogYa <Ghost.Spartan36@gmail.com>
2024-03-15 17:46:52 +0300

    feature: added api modules

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/di/AppModule.kt
@@ -1,4 +1,15 @@
 package ru.sportivityteam.vucmirea.assistant.di
 
+import ru.sportivityteam.vucmirea.assistant.core.ktor.ktorModule
+import ru.sportivityteam.vucmirea.assistant.home.schedule.di.scheduleUseCaseModule
+
 internal fun appModule() =
-    listOf(repositoryModule, useCaseModule, screenModelsModule, platformDatastoreModule)
+    listOf(
+        repositoryModule,
+        useCaseModule,
+        screenModelsModule,
+        platformDatastoreModule,
+        ktorModule,
+        scheduleUseCaseModule,
+        apiModule
+    )
