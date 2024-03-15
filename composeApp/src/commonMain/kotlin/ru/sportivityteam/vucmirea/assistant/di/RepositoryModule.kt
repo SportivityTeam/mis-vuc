18d43718083ee44f61fcf4fbf0e4603e9f7ba6a0
SogYa <Ghost.Spartan36@gmail.com>
2024-03-15 17:48:44 +0300

    feature: added schedule repositories

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/di/RepositoryModule.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/di/RepositoryModule.kt
@@ -3,8 +3,11 @@ package ru.sportivityteam.vucmirea.assistant.di
 import org.koin.dsl.module
 import ru.sportivityteam.vucmirea.assistant.auth.data.AuthRepositoryImpl
 import ru.sportivityteam.vucmirea.assistant.auth.domain.AuthRepository
+import ru.sportivityteam.vucmirea.assistant.home.schedule.data.repository.ScheduleRepositoryImpl
+import ru.sportivityteam.vucmirea.assistant.home.schedule.domain.repository.ScheduleRepository
 
 
 internal val repositoryModule = module {
     single<AuthRepository> { AuthRepositoryImpl(get()) }
+    single<ScheduleRepository> { ScheduleRepositoryImpl(get()) }
 }
