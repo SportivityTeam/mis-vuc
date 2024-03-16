22aaba3f9352787d71bdab2b307e2f1bbf407189
SogYa <Ghost.Spartan36@gmail.com>
2024-03-16 11:57:14 +0300

    refactor: commented for tests

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/schedule/data/api/ScheduleApi.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/schedule/data/api/ScheduleApi.kt
@@ -3,14 +3,12 @@ package ru.sportivityteam.vucmirea.assistant.home.schedule.data.api
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
 
 import ru.sportivityteam.vucmirea.assistant.home.schedule.data.entity.LessonsData
 
 class ScheduleApi(private val httpClient: HttpClient) {
 
     suspend fun getLessonsByGroupId(): LessonsData {
-        val result: LessonsData = httpClient.get("/lessons").body()
-        return result
+        return httpClient.get("/lessons").body<LessonsData>()
     }
 }
