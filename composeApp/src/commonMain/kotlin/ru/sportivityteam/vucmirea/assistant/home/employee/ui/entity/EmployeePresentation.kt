5fd563b96f6629ed17c227861017278fa2954777
SogYa <Ghost.Spartan36@gmail.com>
2024-03-16 11:58:49 +0300

    feat: added mapper

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/entity/EmployeePresentation.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/entity/EmployeePresentation.kt
@@ -12,3 +12,15 @@ data class EmployeePresentation(
     override val audience: String,
     override val type: String
 ) : EmployeeDomain
+
+
+fun EmployeeDomain.toPresentation() = EmployeePresentation(
+    imageUri = imageUri,
+    fullName = fullName,
+    jobTitle = jobTitle,
+    email = email,
+    phone = phone,
+    phoneAdd = phoneAdd,
+    audience = audience,
+    type = type
+)
