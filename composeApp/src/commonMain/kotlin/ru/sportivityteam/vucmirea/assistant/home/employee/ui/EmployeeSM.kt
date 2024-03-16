776a11f96ed84b7ee9f6a5d2bb7b098c6d9bfbd0
SogYa <Ghost.Spartan36@gmail.com>
2024-03-16 11:58:42 +0300

    feat: reworked with firebase

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/EmployeeSM.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/EmployeeSM.kt
@@ -1,9 +1,35 @@
 package ru.sportivityteam.vucmirea.assistant.home.employee.ui
 
+import cafe.adriel.voyager.core.model.screenModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import ru.sportivityteam.vucmirea.assistant.core.uikit.mvi.BaseStateScreenModel
+import ru.sportivityteam.vucmirea.assistant.core.util.State
+import ru.sportivityteam.vucmirea.assistant.home.employee.domain.usecase.GetEmployeeUseCase
+import ru.sportivityteam.vucmirea.assistant.home.employee.ui.entity.toPresentation
 
-class EmployeeSM: BaseStateScreenModel<EmployeeViewState, EmployeeViewAction, EmployeeViewEvent>(initialState = EmployeeViewState()) {
+class EmployeeSM(
+    private val getEmployeeUseCase: GetEmployeeUseCase
+) : BaseStateScreenModel<EmployeeViewState, EmployeeViewAction, EmployeeViewEvent>(initialState = EmployeeViewState()) {
     override fun obtainEvent(viewEvent: EmployeeViewEvent) {
 
+    }
+
+    init {
+        screenModelScope.launch(Dispatchers.IO) {
+            getEmployeeUseCase.invoke().collect {
+                when (it) {
+                    is State.Loading -> viewState = viewState.copy(isLoading = true)
+                    is State.Success -> {
+                        viewState =
+                            viewState.copy(employeeList = it.data!!.map { employee ->
+                                employee!!.toPresentation()
+                            })
+                    }
+
+                    is State.Failed -> println(it.message)
+                }
+            }
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/EmployeeScreen.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/ui/EmployeeScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.koin.getScreenModel
 import ru.sportivityteam.vucmirea.assistant.core.uikit.component.BaseScreen
@@ -18,12 +17,11 @@ class EmployeeScreen : BaseScreen() {
     override fun ScreenContent() {
         val screenModel = getScreenModel<EmployeeSM>()
         val state = screenModel.viewStates().observeAsState()
-        val employeeList = remember { state.value.employeeList }
 
         LazyColumn(
             modifier = Modifier.fillMaxSize().background(color = AssistantTheme.colors.darkBlue)
         ) {
-            items(employeeList) { employee ->
+            items(state.value.employeeList) { employee ->
                 EmployeeCard(employee)
             }
         }
