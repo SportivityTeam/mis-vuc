d0c35a7ecc73b8eae38a2f8ed9fb8df344d14546
SogYa <Ghost.Spartan36@gmail.com>
2024-03-16 11:25:11 +0300

    feature: added serialization

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/data/entity/EmployeeData.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/home/employee/data/entity/EmployeeData.kt
@@ -1,7 +1,9 @@
 package ru.sportivityteam.vucmirea.assistant.home.employee.data.entity
 
+import kotlinx.serialization.Serializable
 import ru.sportivityteam.vucmirea.assistant.home.employee.domain.entity.EmployeeDomain
 
+@Serializable
 data class EmployeeData(
     override val imageUri: String,
     override val fullName: String,
