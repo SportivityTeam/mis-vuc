17f2df8aca6313b5365f5b23e8665fd72782fedd
SogYa <Ghost.Spartan36@gmail.com>
2024-03-19 10:38:41 +0300

    feat: deleted splash screen

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/splash/ui/SplashStates.kt
+++
@@ -1,6 +0,0 @@
-package ru.sportivityteam.vucmirea.assistant.splash.ui
-
-sealed class SplashAction {
-    data object NavigateToHomeScreen : SplashAction()
-    data object NavigateToAuthScreen : SplashAction()
-}
