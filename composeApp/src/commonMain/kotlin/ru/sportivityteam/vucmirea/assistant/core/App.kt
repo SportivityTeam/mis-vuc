4abe14677325a3b809e60c37b8a664963326b322
SogYa <Ghost.Spartan36@gmail.com>
2024-03-19 10:39:32 +0300

    feat: added route to config screen

--- composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/core/App.kt
+++ composeApp/src/commonMain/kotlin/ru/sportivityteam/vucmirea/assistant/core/App.kt
@@ -2,10 +2,10 @@ package ru.sportivityteam.vucmirea.assistant.core
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.Navigator
-import ru.sportivityteam.vucmirea.assistant.splash.ui.SplashScreen
+import ru.sportivityteam.vucmirea.assistant.config.ui.ConfigScreen
 import ru.sportivityteam.vucmirea.assistant.core.uikit.theme.AppTheme
 
 @Composable
 internal fun App() = AppTheme {
-    Navigator(SplashScreen())
+    Navigator(ConfigScreen())
 }
